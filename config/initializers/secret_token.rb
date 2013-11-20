1a5e01d0e07b904df7d8bbd315475c4ab68df669
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2013-11-20 13:51:21 -0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyFinances::Application.config.secret_key_base = '78cc5b5a94f3136e06c8a3b9f4d089e7a7613dc84ef5524f444989223fb8d355b59205314a6d15eb4ba1043547c2c5c2a171fe6ac6acba8ef80bf1a90497d966'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyFinances::Application.config.session_store :cookie_store, key: '_easy_finances_session'
